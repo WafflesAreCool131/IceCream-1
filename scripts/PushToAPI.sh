d16f061dc9a5eb1a40cf0e5b074e75874e192e2c
Pancakse <152734075+Pancaksee@users.noreply.github.com>
2024-01-20 19:38:12 +0000

    [ci skip] fix file name

--- scripts/PushToAPI.sh
+++ scripts/PushToAPI.sh
@@ -20,8 +20,8 @@ promoted=false
 
 number=$(git log --oneline master ^`git describe --tags --abbrev=0` | wc -l)
 changes=$(git log --pretty='%H<<<%s>>>' -"$number" | sed ':a;N;$!ba;s/\n//g')
-jar_name="leaves-$mcversion.jar"
+jar_name="icecream-paperclip-$mcversion-R0.1-SNAPSHOT-reobf.jar"
 jar_sha256=`sha256 $jar_name`
 
-# v2
+# v1
 curl --location --request POST "https://ice-cream-api.vercel.app/v2/commit/build" --header "Content-Type: application/json" --header "Authentication: Bearer $secret" --data-raw "{\"project_id\":\"$project_id\",\"version\":\"$mcversion\",\"channel\":\"$channel\",\"changes\":\"$changes\",\"jar_name\":\"$jar_name\",\"sha256\":\"$jar_sha256\",\"tag\":\"$tag\"}"
